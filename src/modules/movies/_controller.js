7cef5a518fd2b8df8e2c7915d96a715a329f324f
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-01 16:52:51 +0500

    controllerga remove qo'shildi.

--- src/modules/movies/_controller.js
+++ src/modules/movies/_controller.js
@@ -52,3 +52,19 @@ export const showMovies = async (req, res, next) => {
   }
 };
 
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+
+export const removeMovies = async (req, res, next) => {
+  try {
+    const result = await remove({ params: req.params });
+    res.status(200).json({ data: result });
+  } catch (error) {
+    next(error);
+  }
+};
+
