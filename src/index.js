1be0e6c23694ea2813c122725572f61a7e9af948
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-01 08:30:20 +0500

    databazaga chaqirib qo'yish.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { config } from "./shared/config/index.js";
+import { db } from "./db/index.js";
 
 const app = express();
+
+db();
 
 app.listen(config.port, () => {
   console.log(`SERVER HAS BEEN STARTED ON PORT ${config.port}`);
