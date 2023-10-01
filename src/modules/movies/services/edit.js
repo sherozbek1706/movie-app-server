5126047d9a7f1aeddc4f409b469489f0c8842da9
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-01 16:57:09 +0500

    type id xato bersa error berib yuvorsin

--- src/modules/movies/services/edit.js
+++ src/modules/movies/services/edit.js
@@ -4,6 +4,14 @@ import Category from "../../category/models/Category.js";
 import { NotFoundError } from "../../../shared/errors/index.js";
 
 export const edit = async ({ body, params, image }) => {
+  const { type_id, category_id } = body;
+
+  const existing = await Movies.findOne({ _id: params.id, is_deleted: false });
+
+  if (!existing) {
+    throw new NotFoundError("Movies Not Found!");
+  }
+
   return Movies.findByIdAndUpdate(
     { _id: params.id },
     { ...body, image },
