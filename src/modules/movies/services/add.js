648758e2ae11bd36184eaa2a6a093e0f3bd60c74
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-01 16:54:24 +0500

    type_id xato bo'lsa error berilsin.

--- src/modules/movies/services/add.js
+++ src/modules/movies/services/add.js
@@ -4,5 +4,13 @@ import Category from "../../category/models/Category.js";
 import { NotFoundError } from "../../../shared/errors/index.js";
 
 export const add = async ({ body, image }) => {
+  const { type_id, category_id } = body;
+
+  const existingType = await Type.findOne({ _id: type_id, is_deleted: false });
+
+  if (!existingType) {
+    throw new NotFoundError("Type Not Found!");
+  }
+
   return Movies.create({ ...body, image });
 };
