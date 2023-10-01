a116378c3aec4a29287eee4e230982617c289541
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-01 08:45:30 +0500

    So'rov noto'g'ri yuborilganda xato beradi.

---
+++ src/shared/errors/index.js
@@ -0,0 +1,9 @@
+export class BadRequestError extends Error {
+  constructor(msg) {
+    super(msg);
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, BadRequestError);
+    }
+  }
+}
