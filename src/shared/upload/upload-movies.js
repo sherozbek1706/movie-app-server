e63ee8faa0626ba3b9a4f8f6895b7ab1fcacc671
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-01 16:33:03 +0500

    Agar qandaydir filenameda xato bo'lsa invalid berib yuboradi.

--- src/shared/upload/upload-movies.js
+++ src/shared/upload/upload-movies.js
@@ -9,3 +9,22 @@ const storage = multer.diskStorage({
   },
 });
 
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    const allowedExtensions = [".jpg", ".jpeg", ".png"];
+    const fileExtension = path.extname(file.originalname);
+
+    if (allowedExtensions.includes(fileExtension)) {
+      cb(null, true);
+    } else {
+      cb(new Error("Invalid file type"));
+    }
+  },
+  limits: {
+    files: 1,
+    fileSize: 1024 * 1024 * 1024,
+  },
+}).single("image");
+
+export default upload;
