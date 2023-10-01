74dc2a43855b7067b84277d2ba3eae3b69cc14d7
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-01 11:47:55 +0500

    swagger category uchun

---
+++ src/shared/swagger/swagger.js
@@ -0,0 +1,19 @@
+import swaggerAutogen from "swagger-autogen";
+
+const doc = {
+  info: {
+    title: "My API",
+    description: "Description",
+  },
+  host: "localhost:3000",
+};
+
+const outputFile = "./swagger-output.json";
+const routes = ["../../modules/category/_api.js"];
+
+/* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
+root file where the route starts, such as index.js, app.js, routes.js, etc ... */
+
+swaggerAutogen()(outputFile, routes, doc).then(async () => {
+  await import("../../index.js"); // Your project's root file
+});
