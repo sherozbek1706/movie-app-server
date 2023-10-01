5b90cdaee6ed7eb6338024ec6dff4c80319e1172
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-01 08:47:32 +0500

    handle errorni ro'yxatdan o'tkazildi.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import { db } from "./db/index.js";
 
 const app = express();
 
+// import handleError
+import handleError from "./shared/errors/handle.js";
+
 db();
+
+// Middleware Error
+app.use(handleError);
 
 app.listen(config.port, () => {
   console.log(`SERVER HAS BEEN STARTED ON PORT ${config.port}`);
