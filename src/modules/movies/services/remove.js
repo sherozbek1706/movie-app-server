415f991ffb417ea4f0e6035ced279ccc09800ff7
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-01 16:59:00 +0500

    Agar id mos kelmasa xato berib yubor.

--- src/modules/movies/services/remove.js
+++ src/modules/movies/services/remove.js
@@ -2,6 +2,14 @@ import { NotFoundError } from "../../../shared/errors/index.js";
 import Movies from "../models/Movies.js";
 
 export const remove = async ({ params }) => {
+  const existing = await Movies.findOne({
+    _id: params.id,
+    is_deleted: false,
+  });
+  if (!existing) {
+    throw new NotFoundError("Movies Not Found!");
+  }
+
   return Movies.findByIdAndUpdate(
     params.id,
     { is_deleted: true },
