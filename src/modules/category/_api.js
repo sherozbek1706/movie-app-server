c3a4fcd8523ff5ad0d93dc0445aca7449d010f0a
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-01 10:37:03 +0500

    category o'chirish uchun route tuzildi.

--- src/modules/category/_api.js
+++ src/modules/category/_api.js
@@ -3,11 +3,13 @@ import {
   addCategory,
   editCategory,
   listCategory,
+  removeCategory,
 } from "./_controller.js";
 const router = Router();
 
 router.post("/category", addCategory);
 router.get("/category", listCategory);
+router.delete("/category/:id", removeCategory);
 router.patch("/category/:id", editCategory);
 
 export default router;
