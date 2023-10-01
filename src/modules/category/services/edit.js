8847aa8033df8fa2c960deb153826021ed2809d9
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-01 10:43:46 +0500

    Agar category topilmasa xato beradi

--- src/modules/category/services/edit.js
+++ src/modules/category/services/edit.js
@@ -2,6 +2,13 @@ import { NotFoundError } from "../../../shared/errors/index.js";
 import Category from "../models/Category.js";
 
 export const edit = async ({ params, body }) => {
+  const existing = await Category.findOne({
+    _id: params.id,
+    is_deleted: false,
+  });
+  if (!existing) {
+    throw new NotFoundError("Category Not Found!");
+  }
 
   return Category.findByIdAndUpdate(params.id, { ...body }, { new: true });
 };
