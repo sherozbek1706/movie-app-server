7670be9f3d86fcaee373e5241c67bc3559a2e9d0
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-01 12:25:37 +0500

    routerga route yozildi, o'chirish uchun controller bilan ishlaydi.

--- src/modules/type/_api.js
+++ src/modules/type/_api.js
@@ -3,11 +3,13 @@ import {
   addType,
   editType,
   listType,
+  removeType,
 } from "./_controller.js";
 const router = Router();
 
 router.post("/type", addType);
 router.get("/type", listType);
+router.delete("/type/:id", removeType);
 router.patch("/type/:id", editType);
 
 export default router;
