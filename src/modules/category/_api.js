bc6be26032a969df835b2de5828886876eab3e10
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-01 10:36:51 +0500

    category tahrirlash uchun route tuzildi.

--- src/modules/category/_api.js
+++ src/modules/category/_api.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import {
   addCategory,
+  editCategory,
   listCategory,
 } from "./_controller.js";
 const router = Router();
 
 router.post("/category", addCategory);
 router.get("/category", listCategory);
+router.patch("/category/:id", editCategory);
+
 export default router;
