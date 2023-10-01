316811c1a573d261d61eec3ca6f0cfc1a6a06bef
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-01 12:25:13 +0500

    routerga route yozildi, tahrirlash uchun controller bilan ishlaydi.

--- src/modules/type/_api.js
+++ src/modules/type/_api.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import {
   addType,
+  editType,
 } from "./_controller.js";
 const router = Router();
 
 router.post("/type", addType);
+router.patch("/type/:id", editType);
 
 export default router;
