adea087ef4f29630c33afc9cea614dfbf9b4219b
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-01 09:03:44 +0500

    public papka static qilindi.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import express from "express";
 import { config } from "./shared/config/index.js";
 import { db } from "./db/index.js";
+import cors from "cors";
+
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 // import handleError
 import handleError from "./shared/errors/handle.js";
+
+// public fayl'ni public qilish yo'li
+app.use("/files", express.static(path.join(__dirname, "public")));
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 db();
 
