dc67c85134669cd2679b8a11712af8544faca7c8
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-01 10:36:35 +0500

    category ko'rish uchun route tuzildi.

--- src/modules/category/_api.js
+++ src/modules/category/_api.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import {
   addCategory,
+  listCategory,
 } from "./_controller.js";
 const router = Router();
 
 router.post("/category", addCategory);
+router.get("/category", listCategory);
 export default router;
