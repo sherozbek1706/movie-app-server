f31e51bdd9e1d608863abc7237669de046de18b6
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-01 10:45:45 +0500

    agar category topilmasa xate berib yuboradi.

--- src/modules/category/services/remove.js
+++ src/modules/category/services/remove.js
@@ -2,6 +2,14 @@ import { NotFoundError } from "../../../shared/errors/index.js";
 import Category from "../models/Category.js";
 
 export const remove = async ({ params }) => {
+  const existing = await Category.findOne({
+    _id: params.id,
+    is_deleted: false,
+  });
+  if (!existing) {
+    throw new NotFoundError("Category Not Found!");
+  }
+
   return Category.findByIdAndUpdate(
     params.id,
     { is_deleted: true },
