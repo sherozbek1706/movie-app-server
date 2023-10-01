3920e4a06de082c49ea7be2a365abff4b73f7e8e
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-01 08:30:03 +0500

    Ulanganda elon berish va ulanmaganda elon berish.

--- src/db/index.js
+++ src/db/index.js
@@ -4,4 +4,10 @@ import { config } from "../shared/config/index.js";
 export const db = () => {
   mongoose
     .connect(`mongodb://${config.db.host}:${config.db.port}/${config.db.name}`)
+    .then(() => {
+      console.log("SERVER HAS BEEN CONNECTED TO DATABASE SUCCESSFULL");
+    })
+    .catch(() => {
+      console.log("SERVER CAN NOT CONNECT TO DATABASE");
+    });
 };
