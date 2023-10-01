cdf2ca71c577da2ecf0e58e77a593d1cd6a090c7
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-01 12:30:38 +0500

    controller yozib chiqildi, type'larni qaytarish uchun

--- src/modules/type/_controller.js
+++ src/modules/type/_controller.js
@@ -49,3 +49,19 @@ export const removeType = async (req, res, next) => {
   }
 };
 
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+export const unremoveType = async (req, res, next) => {
+  try {
+    const result = await unremove({ params: req.params });
+
+    res.status(200).json({ data: result });
+  } catch (error) {
+    next(error);
+  }
+};
+
