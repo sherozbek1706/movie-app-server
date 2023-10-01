83117ae7d6c34d2ef9e527f73fe68ab354d2f97a
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-01 12:25:48 +0500

    routerga route yozildi, qaytarish uchun controller bilan ishlaydi.

--- src/modules/type/_api.js
+++ src/modules/type/_api.js
@@ -4,12 +4,14 @@ import {
   editType,
   listType,
   removeType,
+  unremoveType,
 } from "./_controller.js";
 const router = Router();
 
 router.post("/type", addType);
 router.get("/type", listType);
 router.delete("/type/:id", removeType);
+router.delete("/type/un/:id", unremoveType);
 router.patch("/type/:id", editType);
 
 export default router;
