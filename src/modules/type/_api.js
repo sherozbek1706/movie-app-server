b914293ec4e1d28054f8e15aea6d22b59ed20eb3
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-01 12:25:23 +0500

    routerga route yozildi, ko'rish uchun controller bilan ishlaydi.

--- src/modules/type/_api.js
+++ src/modules/type/_api.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import {
   addType,
   editType,
+  listType,
 } from "./_controller.js";
 const router = Router();
 
 router.post("/type", addType);
+router.get("/type", listType);
 router.patch("/type/:id", editType);
 
 export default router;
