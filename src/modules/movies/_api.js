aba561cef38efdfb5409e23ffe922698abee0395
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-01 16:50:01 +0500

    routerga put qildim.

--- src/modules/movies/_api.js
+++ src/modules/movies/_api.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import {
   addMovies,
+  editMovies,
 } from "./_controller.js";
 import upload from "../../shared/upload/upload-movies.js";
 const router = Router();
 
 const mAddMovies = [upload];
+const mEditMovies = [upload];
 
 router.post("/movies", mAddMovies, addMovies);
+router.put("/movies/:id", mEditMovies, editMovies);
 
 export default router;
