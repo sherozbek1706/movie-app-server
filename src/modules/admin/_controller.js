7bc78c710d1e64f8f4dc86cdabfb4657f0126587
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-02 12:34:25 +0500

    Adminlarni o'chirish bo'yicha controller. So'rovlarni resulga qabul qiladi.

--- src/modules/admin/_controller.js
+++ src/modules/admin/_controller.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   add,
   list,
+  remove,
   show,
 } from "./services/_index.js";
 
@@ -49,3 +50,19 @@ export const showAdmin = async (req, res, next) => {
     next(error);
   }
 };
+
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+export const removeAdmin = async (req, res, next) => {
+  try {
+    const result = await remove({ params: req.params });
+    res.status(200).json({ data: result });
+  } catch (error) {
+    next(error);
+  }
+};
+
