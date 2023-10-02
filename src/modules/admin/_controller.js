eab254b80733d5ed682d232adb9660e50e234330
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-02 12:34:05 +0500

    Adminlarni alohida ko'rsatish bo'yicha controller. So'rovlarni resulga qabul qiladi.

--- src/modules/admin/_controller.js
+++ src/modules/admin/_controller.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   add,
   list,
+  show,
 } from "./services/_index.js";
 
 /**
@@ -34,3 +35,17 @@ export const listAdmin = async (req, res, next) => {
   }
 };
 
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+export const showAdmin = async (req, res, next) => {
+  try {
+    const result = await show({ params: req.params });
+    res.status(200).json({ data: result });
+  } catch (error) {
+    next(error);
+  }
+};
