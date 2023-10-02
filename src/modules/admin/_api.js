735713351f7546f1038194282bbee6687aa527fa
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-02 12:38:17 +0500

    routerga controller ulandi va middleware  admin o'chirish yo'liga berib yuborildi.

--- src/modules/admin/_api.js
+++ src/modules/admin/_api.js
@@ -5,14 +5,17 @@ import {
   editAdmin,
   listAdmin,
   loginAdmin,
+  removeAdmin,
 } from "./_controller.js";
 
 const router = Router();
 
+const mRemoveAdmin = [isLoggedIn, isMongoId];
 const mEditAdmin = [isLoggedIn, isMongoId];
 
 router.post("/admin/add", isLoggedIn, addAdmin);
 router.get("/admin", isLoggedIn, listAdmin);
+router.delete("/admin/:id", mRemoveAdmin, removeAdmin);
 router.put("/admin/:id", mEditAdmin, editAdmin);
 router.post("/admin/login", loginAdmin);
 
