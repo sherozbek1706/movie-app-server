ca3858b93679768c73101e6e521d1c47c65be46c
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-02 12:37:19 +0500

    routerga controller ulandi va middleware  admin ko'rish yo'liga berib yuborildi.

--- src/modules/admin/_api.js
+++ src/modules/admin/_api.js
@@ -3,6 +3,7 @@ import { isLoggedIn, isMongoId } from "../../shared/auth/_index.js";
 import {
   addAdmin,
   editAdmin,
+  listAdmin,
 } from "./_controller.js";
 
 const router = Router();
@@ -10,6 +11,7 @@ const router = Router();
 const mEditAdmin = [isLoggedIn, isMongoId];
 
 router.post("/admin/add", isLoggedIn, addAdmin);
+router.get("/admin", isLoggedIn, listAdmin);
 router.put("/admin/:id", mEditAdmin, editAdmin);
 
 export default router;
