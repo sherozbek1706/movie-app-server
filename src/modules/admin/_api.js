e1c13146c6bab9771803f0c2bc054d50f085e787
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-02 12:38:49 +0500

    routerga controller ulandi va middleware  admin alohida ko'rsatish yo'liga berib yuborildi.

--- src/modules/admin/_api.js
+++ src/modules/admin/_api.js
@@ -6,17 +6,20 @@ import {
   listAdmin,
   loginAdmin,
   removeAdmin,
+  showAdmin,
   unremoveAdmin,
 } from "./_controller.js";
 
 const router = Router();
 
+const mShowAdmin = [isLoggedIn, isMongoId];
 const mRemoveAdmin = [isLoggedIn, isMongoId];
 const mUnRemoveAdmin = [isLoggedIn, isMongoId];
 const mEditAdmin = [isLoggedIn, isMongoId];
 
 router.post("/admin/add", isLoggedIn, addAdmin);
 router.get("/admin", isLoggedIn, listAdmin);
+router.get("/admin/:id", mShowAdmin, showAdmin);
 router.delete("/admin/:id", mRemoveAdmin, removeAdmin);
 router.delete("/admin/un/:id", mUnRemoveAdmin, unremoveAdmin);
 router.put("/admin/:id", mEditAdmin, editAdmin);
