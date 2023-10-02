fb3ea9a432a60c1105a248a635672d0eabbb55db
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-02 12:34:55 +0500

    Adminlarni qaytarish bo'yicha controller. So'rovlarni resulga qabul qiladi.

--- src/modules/admin/_controller.js
+++ src/modules/admin/_controller.js
@@ -4,6 +4,7 @@ import {
   list,
   remove,
   show,
+  unremove,
 } from "./services/_index.js";
 
 /**
@@ -60,6 +61,21 @@ export const showAdmin = async (req, res, next) => {
 export const removeAdmin = async (req, res, next) => {
   try {
     const result = await remove({ params: req.params });
+    res.status(200).json({ data: result });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+export const unremoveAdmin = async (req, res, next) => {
+  try {
+    const result = await unremove({ params: req.params });
     res.status(200).json({ data: result });
   } catch (error) {
     next(error);
