d7ee0bca759d402eb5ba649841abd0285890d9c1
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-02 12:44:20 +0500

    username topilmasa xato qaytarsin.

--- src/modules/admin/services/login.js
+++ src/modules/admin/services/login.js
@@ -7,5 +7,12 @@ import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { config } from "../../../shared/config/index.js";
 export const login = async ({ body }) => {
+  const { username, password } = body;
+
+  const exist_username = await Admin.findOne({ username });
+  if (!exist_username) {
+    throw new NotFoundError("This Username Not Found!");
+  }
+
   return { token };
 };
