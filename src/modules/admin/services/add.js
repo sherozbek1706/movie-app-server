23ece09e7515b2a02cdafc252875bd821563ca88
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-02 12:42:28 +0500

    Agar username oldin ishlatilgan bo'lsa xato berilsin.

--- src/modules/admin/services/add.js
+++ src/modules/admin/services/add.js
@@ -3,5 +3,12 @@ import Admin from "../models/Admin.js";
 import bcrypt from "bcryptjs";
 
 export const add = async ({ body }) => {
+  const { username, password } = body;
+
+  const existing = await Admin.findOne({ username });
+  if (existing) {
+    throw new BadRequestError("This username already exist!");
+  }
+
   return Admin.create({ ...body, password: hashedPassword });
 };
