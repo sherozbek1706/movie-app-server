7d5a8b6dcb3ffde7b378c7e5637a62c5e699e83e
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-02 12:33:38 +0500

    Adminlarni list bo'yicha controller. So'rovlarni resulga qabul qiladi.

--- src/modules/admin/_controller.js
+++ src/modules/admin/_controller.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   add,
+  list,
 } from "./services/_index.js";
 
 /**
@@ -13,6 +14,21 @@ export const addAdmin = async (req, res, next) => {
   try {
     const result = await add({ body: req.body });
     res.status(201).json({ data: result });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+export const listAdmin = async (req, res, next) => {
+  try {
+    const result = await list();
+    res.status(200).json({ data: result });
   } catch (error) {
     next(error);
   }
