f6a2e8388f81c7dde3b2cf447feea08b2d4262a7
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-02 12:43:20 +0500

    Agar Admin topilmasa xato berilsin.

--- src/modules/admin/services/edit.js
+++ src/modules/admin/services/edit.js
@@ -3,6 +3,13 @@ import Admin from "../models/Admin.js";
 import bcrypt from "bcryptjs";
 
 export const edit = async ({ params, body }) => {
+  const { username, password, ...data } = body;
+
+  const existing = await Admin.findOne({ _id: params.id, is_deleted: false });
+
+  if (!existing) {
+    throw new NotFoundError("Admin Not Found!");
+  }
 
   const exis_username = await Admin.findOne({ username });
   if (existing.username != username && exis_username) {
