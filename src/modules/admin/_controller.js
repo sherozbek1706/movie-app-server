f68f6e924b0c4e646c77dd8782dbd572ad831d84
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-02 12:35:19 +0500

    Adminlarni login bo'yicha controller. So'rovlarni resulga qabul qiladi.

--- src/modules/admin/_controller.js
+++ src/modules/admin/_controller.js
@@ -3,6 +3,7 @@ import {
   add,
   edit,
   list,
+  login,
   remove,
   show,
   unremove,
@@ -98,3 +99,17 @@ export const editAdmin = async (req, res, next) => {
   }
 };
 
+/**
+ *
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
+export const loginAdmin = async (req, res, next) => {
+  try {
+    const result = await login({ body: req.body });
+    res.status(200).json({ data: result });
+  } catch (error) {
+    next(error);
+  }
+};
