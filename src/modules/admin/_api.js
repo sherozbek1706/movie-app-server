b0778fe4bb1dcce4194734ae9f42a1d0352f6dcf
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-02 12:38:29 +0500

    routerga controller ulandi va middleware  admin qaytarish yo'liga berib yuborildi.

--- src/modules/admin/_api.js
+++ src/modules/admin/_api.js
@@ -6,16 +6,19 @@ import {
   listAdmin,
   loginAdmin,
   removeAdmin,
+  unremoveAdmin,
 } from "./_controller.js";
 
 const router = Router();
 
 const mRemoveAdmin = [isLoggedIn, isMongoId];
+const mUnRemoveAdmin = [isLoggedIn, isMongoId];
 const mEditAdmin = [isLoggedIn, isMongoId];
 
 router.post("/admin/add", isLoggedIn, addAdmin);
 router.get("/admin", isLoggedIn, listAdmin);
 router.delete("/admin/:id", mRemoveAdmin, removeAdmin);
+router.delete("/admin/un/:id", mUnRemoveAdmin, unremoveAdmin);
 router.put("/admin/:id", mEditAdmin, editAdmin);
 router.post("/admin/login", loginAdmin);
 
