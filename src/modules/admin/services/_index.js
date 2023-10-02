435850be97c58f07bd202ab4581ea2518136b758
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-02 12:39:36 +0500

    Barcha servicelar export qilib yuborildi.

---
+++ src/modules/admin/services/_index.js
@@ -0,0 +1,7 @@
+export * from "./add.js";
+export * from "./list.js";
+export * from "./show.js";
+export * from "./remove.js";
+export * from "./unremove.js";
+export * from "./edit.js";
+export * from "./login.js";
