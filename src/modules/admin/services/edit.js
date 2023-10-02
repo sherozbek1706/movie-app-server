89a861e3e149a18d885ced19e2f4cc940fc7a542
Baxtiyorov She'rozbek <91471554+sherozbek1706@users.noreply.github.com>
2023-10-02 12:42:40 +0500

    Agar username oldin ishlatilgan bo'lsa xato berilsin.

--- src/modules/admin/services/edit.js
+++ src/modules/admin/services/edit.js
@@ -3,6 +3,12 @@ import Admin from "../models/Admin.js";
 import bcrypt from "bcryptjs";
 
 export const edit = async ({ params, body }) => {
+
+  const exis_username = await Admin.findOne({ username });
+  if (existing.username != username && exis_username) {
+    throw new BadRequestError("This username already exist!");
+  }
+
   return Admin.findByIdAndUpdate(
     params.id,
     { ...body, password: bcrypt.hashSync(password, 10) },
